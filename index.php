19006d2ed330bf90635fe0b044eab2548b747617
Abubakr <developer@Developers-MacBook-Pro.local>
2020-11-06 12:13:24 +0500

    Intro to PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  // Variables
+  // $startsWithDollarSign = "text";// $
+  // $a = 3;
+  // $2a = 3; //gives error
+  // a = 3; //gives error
+  // $a-b = 3; // gives error
+
+  // echo $startsWithDollarSign;
+  // echo "<br>";
+  // echo $a;
+  //
+  // $x = 5;
+  // $y = array("x" => "Hello");
+  // echo "<br>";
+  // print $y[x];
+
+  // Strings
+  $someString1 = "double quotation";
+  $someString2 = 'single quotation';
+  echo $someString1;
+  echo "<br>" . '<br>';
+  echo $someString2;
+
+  echo "<br>Weather " . "is cool";
