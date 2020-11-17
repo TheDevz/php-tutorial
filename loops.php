d5edbc76e9415b3cd4ee02ecf96c02dc46cd94bd
abdvkh <e-address@bk.ru>
2020-11-17 22:01:14 +0500

    Numbers fully covered;

--- loops.php
+++ loops.php
@@ -42,17 +42,107 @@ echo "Loops.<br>";
 // for ($counter = 0; $counter < 10; $counter++) {
 // 	echo "$counter. Wake up!<br>";
 // }
-// defines counter which is 0
-// checks codition(0 < 10)[it's true], if its true it runs given block of code
-// // prints out "0. Wake up!<br>"
-// increments counter value($counter++ == $counter = $counter + 1)[$counter = 1]
-// checks codition(1 < 10)[it's true], if its true it runs given block of code
-// // prints out "1. Wake up!<br>"
-// increments counter value($counter++ == $counter = $counter + 1)[$counter = 2]
-// checks codition(2 < 10)[it's true], if its true it runs given block of code
-// // prints out "2. Wake up!<br>"
 
+// Pseudo-code
+//s1 defines counter which is 0
+//s2 checks codition(0 < 10)[it's true], if its true it runs given block of code
+//s3 // prints out "0. Wake up!<br>"
+//s4 increments counter value($counter++ == $counter = $counter + 1)[$counter = 1]
+//s5 checks codition(1 < 10)[it's true], if its true it runs given block of code
+//s6 // prints out "1. Wake up!<br>"
+//s7 inśrements counter value($counter++ == $counter = $counter + 1)[$counter = 2]
+//s8 checks codition(2 < 10)[it's true], if its true it runs given block of code
+//s9 // prints out "2. Wake up!<br>"
+
+// ['A', 'b', 'u', ...]
+//		 0123456
 // $name = "Abubakr";
-// for ($i=0; $i < ; $i++) {
+// for ($i = 0; $i < strlen($name); $i++) {
 // 	echo $name[$i] . "<br>";
+// }
+
+# foreach
+
+/* DO NOT DO IT
+
+// $name = "Alex";
+
+// foreach ($name as $character) {
+// 	echo $character . "<br>";
+// }
+
+ */
+
+// //			   0	   1	 3
+// $students_name = ['John', 'Doe', 'Alex'];
+
+// //		data struct.  value
+// foreach ($students_name as $name) {
+// 	echo $name . "<br>";
+// }
+
+// //			   0	   1	 3
+// $students = ['John', 'Doe', 'Alex'];
+
+// //		data struct.	key 	value
+// foreach ($students as $index => $name) {
+// 	echo "$index. $name" . "<br>";
+// }
+
+// //			   	   key	    val.  key 	  val.   key      val.
+// $students_age = ['John' => '18', 'Doe' => '20', 'Alex' => '19'];
+
+// //		 data struct.	   key 	   value
+// foreach ($students_age as $name => $age) {
+// 	echo "$name is $age y.o." . "<br>";
+// }
+
+# break
+
+// for ($x = 0; $x < 10; $x++) {
+
+// 	if ($x == 4) {
+// 		break;
+// 	}
+// 	echo "The number is: $x <br>";
+// }
+
+# continue
+
+// for ($x = 0; $x < 10; $x++) {
+
+// 	if ($x == 4) {
+// 		continue;
+// 	}
+// 	echo "The number is: $x <br>";
+
+// }
+
+# break & continue in while
+
+// $x = 0;
+
+// while ($x < 10) {
+
+// 	if ($x == 4) {
+// 		$x++;
+// 		continue;
+// 	}
+
+// 	echo "The number is: $x <br>";
+// 	$x++;
+// }
+
+// $x = 0;
+// while ($x < 10) {
+// 	if ($x == 2) {
+// 		$x++;
+// 		continue;
+// 	}
+
+// 	if ($x == 4) {
+// 		break;
+// 	}
+// 	echo "The number is: $x <br>";
+// 	$x++;
 // }
--- numbers.php
+++ numbers.php
@@ -1,12 +1,90 @@
 <?php
+echo "<h1>Numbers</h1><br>";
 
 # Declaring numbers
+$a = 4.0;
+$b = 1;
+
+echo "\$a = $a" . "<br>";
+echo "\$b = $b" . "<br>";
 
 # Arithmetic operators
+// echo "$a + $b = " . $a + $b . "<br>";
+// echo "$a - $b = " . $a - $b . "<br>";
+// echo "$a * $b = " . $a * $b . "<br>";
+// echo "$a / $b = " . $a / $b . "<br>";
+// echo "$a % $b = " . $a % $b . "<br>";
 
 # Assignment with math operators
+// echo "$a += $b" . $a += $b . "<br>";
+// echo "$a -= $b" . $a -= $b . "<br>";
+// echo "$a *= $b" . $a *= $b . "<br>";
+// echo "$a /= $b" . $a /= $b . "<br>";
+// echo "$a %= $b" . $a %= $b . "<br>";
 
 # Number checking operators
+// integer
+echo "<pre>";
+echo "is_integer(\$b) && is_int(\$b) && is_long(\$b) is "; // is_int(); // is_long(); # are the same
+var_dump(is_integer($b));
+echo "</pre>";
+
+echo "<br>";
+
+// float
+echo "<pre>";
+echo "is_float(\$a) && is_double(\$a) is "; // is_double(\$a); # is the same
+var_dump(is_float($a));
+echo "</pre>";
+
+// infinite & finite
+echo "<pre>";
+echo "is_infinite(\$a) is ";
+var_dump(is_infinite($a));
+
+echo "<br>";
+
+echo "is_finite(\$a) is ";
+var_dump(is_finite($a));
+echo "</pre>";
+
+echo "<pre>";
+echo "MAXIMUM PHP available number is ";
+var_dump(PHP_FLOAT_MAX);
+echo "</pre>";
+
+// NaN(Not a number)
+echo "<pre>";
+echo "<br>";
+echo "is_nan(\$a) is ";
+var_dump(is_nan($a));
+echo "</pre>";
+
+// Numeric
+echo "<pre>";
+echo "<br>";
+echo "is_numeric(\"a12343\") is ";
+var_dump(is_numeric("a12343"));
+echo "</pre>";
+
+# PHP Casting Strings and Floats to Integers
+
+// Cast float to int
+$x = 23465.768;
+$int_cast = (int) $x;
+echo $int_cast;
+
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+$int_cast = (int) $x;
+echo $int_cast;
+
+// Cast string to float
+$x = "23465.768";
+$int_cast = (float) $x;
+echo $int_cast;
 
 # Increment/Decrement operators
 
