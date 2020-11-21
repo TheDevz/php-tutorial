01063ab8abd504d38609dfd81e30f254d0eb3924
abdvkh <e-address@bk.ru>
2020-11-21 21:41:40 +0500

    Strings done; Arrays almost done

--- arrays.php
+++ arrays.php
@@ -1,40 +1,136 @@
 <?php
 
-## Create an array
+## Create(declaration and definition) an array
+// declaration is creation: array() or []
+$arr_1way_declaration = array('Cat', 'Dog');
+$arr_1way_definition = array(
+	0 => 'Cat', 1 => 'Dog',
+);
+
+//							  0		 1
+// $arr_2way_declaration = ['Cat', 'Dog'];
+// $arr_2way_definition = [
+// 	0 => 'Cat', 1 => 'Dog',
+// ];
+
+// var_dump($arr_1way_declaration === $arr_2way_definition);
 
 ## Print the whole array
+// print_r($arr_1way_definition);
+
+// foreach ($arr_1way_definition as $key => $value) {
+// 	echo "$key. $value <br>";
+// }
+
+// var_dump($arr_1way_definition);
+
+// for ($i = 0; $i < count($arr_1way_definition); $i++) {
+// 	echo "$i. " . $arr_1way_definition[$i] . "<br>";
+// }
 
 ## Get element by index
+// echo $arr_1way_definition[0];
 
 ## Set element by index
+$animals = ['Lion', 'Cat', 'Dog'];
+var_dump($animals);
+
+echo "<br>";
+
+$animals[2] = "Puma";
+var_dump($animals);
+
+echo "<br>";
+
+$animals[3] = "Leopard";
+var_dump($animals);
 
 ## Check if an array has element at index 2
+echo "<br>Is \$animals[4] exists? Answer: ";
+// var_dump(isset($animals[4]));
+var_dump(array_key_exists(4, $animals));
 
 ## Append element
+echo "<br>";
+
+$animals[] = 'Dog';
+var_dump($animals);
+
+echo "<br>";
 
 ## Print the length of the array
 
+echo "<br>We have " . count($animals) . " animals in the zoo";
+
 ## Add the element at the end of an array
+$girr = "Girraffe";
+array_push($animals, $girr);
+var_dump($animals);
 
 ## Remove the element from the end of an array
+echo "<br>";
+array_pop($animals);
+var_dump($animals);
 
-## Add the element at the beginning of an array
+## Add the element at the beginning of an array(array_unshift)
+echo "<br>";
+array_unshift($animals, 'Horse', 'Goat', 'Cow');
+var_dump($animals);
 
-## Remove the element from the beginning of an array
+## Remove the element from the beginning of an array(array_shift)
+echo "<br>";
+array_shift($animals);
+var_dump($animals);
 
-## Split the string into an array
+## Split the string into an array(explode)
+//									0		1	  2		 3
+$pizza_order_ingredients_input = "Tomato, onion, meat, mushrooms";
+$ingredients = explode(', ', $pizza_order_ingredients_input);
+echo "<br>";
+var_dump($ingredients);
 
-## Combine array elements into a string
+## Combine array elements into a string(implode)
+echo "<br>";
+$string_animals = implode(', ', $animals);
+var_dump($string_animals);
 
-## Check if an element exists within the array
+## Check if an element exists within the array(in_array)
+echo "<br>Is 'Horse' in the array? Answer: ";
+var_dump(in_array('Horse', $animals));
 
-## Search element index in the array
+## Search element index in the array(array_search)
+echo "<br>Find me the 'Cat'. Its index is ";
+var_dump(array_search('Cat', $animals));
 
-## Merge two arrays
+## Merge two arrays(array_merge)
+echo "<br>";
+$merging_arrays_1way = array_merge($animals, $ingredients);
+var_dump($merging_arrays_1way);
+
+echo "<br>";
+$merging_arrays_2way = [...$animals, ...$ingredients];
+var_dump($merging_arrays_2way);
 
 ## Sorting of an array(Reverse order also)
+// alphabet simple sort
+echo "<br>";
+sort($animals);
+var_dump($animals);
+
+// alphabet reverse sort
+echo "<br>";
+rsort($animals);
+var_dump($animals);
+
+// key reverse sort
+echo "<br>";
+krsort($animals);
+var_dump($animals);
 
 ## Filter, reduce, map of an array
+function multiply_on_itself($array_element) {
+	return $array_element * $array_element;
+}
 
 /* http://php.net/manual/en/ref.array.php */
 
--- strings.php
+++ strings.php
@@ -1,13 +1,120 @@
 <?php
 
 # Create simple string
+//			['H', 'e', 'l', 'l', ...]
+$hello = "Hello";
+//		  012345
+$world = "world!";
 
 # String concatenation
+echo $hello . " " . $world;
+echo "<br>How " . " r u?";
 
 # String functions
 
+## get the length of the string
+echo "<br>'Hello' consists of " . strlen($hello) . " characters"; //5
+echo "<br>";
+
+## get rid of spaces from sides
+
+// echo "<br>";
+// $spaced_word = "      Some text with space        ";
+// echo '<br>' . '<p>' . $spaced_word . '</p>';
+// $word_without_spaces = trim($spaced_word);
+// echo '<br>' . $word_without_spaces;
+
+$word = "     hot   ";
+
+if (trim($word) === 'hot') {
+	echo "No need of jacket";
+} else {
+	echo "Wear jacket";
+}
+
+// ltrim($word); // removes spaces from left side
+// rtrim($word); // removes spaces from right side
+
+## counting words of a string
+$some_text = "Loren ipsum domeo ...etc";
+echo "<br> [$some_text] consists of " . str_word_count($some_text) . " words";
+
+## reversing the string
+$normal_word = "armstrong";
+$reversed_word = strrev($normal_word);
+echo '<br>strrev($normal_word) === ' . $reversed_word; //gnortsmra
+echo "<br>strrev(123456) === " . strrev(123456); //654321
+
+## UPPERCASE & lowercase of string
+echo "<br>" . strtoupper($hello); // HELLO
+echo "<br>" . strtolower($hello); // hello
+
+$word = "HOt";
+
+if (strtolower($word) /*hot*/ === 'hot') {
+	echo "<br>No need of jacket";
+} else {
+	echo "<br>Wear jacket";
+}
+
+## UPPERCASE & lowercase of 1st character of a string
+echo '<br>' . lcfirst($hello); // hello
+echo '<br>' . ucfirst($world); // World!
+
+$firstname = "abuBAKr"; //-> Abubakr
+echo "<br>" . $firstname;
+echo "<br>" . ucfirst(strtolower($firstname));
+
+## position of the given character within the string
+//Find the position of the first occurrence of a substring in a string
+$indexOfWorld = strpos($world, " world");
+if ($indexOfWorld === false) {
+	echo "<br>This string doesn't contain ' world' word";
+}
+echo "<br>" . $indexOfWorld;
+
+## returns substring of a string
+$contacts = "EduTheDevs, info@edu.thedevs.co";
+$email = substr($contacts, 12);
+$name = substr($contacts, 0, 10);
+echo "<br>Name is " . $name;
+echo "<br>Email is " . $email;
+
+## replacing specific string by the given string
+$details = "<br>Name is %n, Email is %e";
+echo "<br>" . str_replace('domeo', 'romeo', $some_text);
+$details_with_name = str_replace('%n', $name, $details);
+echo $details_with_name;
+echo str_replace('%e', $email, $details_with_name);
+
+## case insensitive string replacement
+$details = "<br>Name is %N, Email is %e";
+echo "<br>" . str_ireplace('domeo', 'romeo', $some_text);
+$details_with_name = str_ireplace('%n', $name, $details);
+echo $details_with_name;
+echo str_ireplace('%e', $email, $details_with_name);
+
+## repeating the given string the give times
+echo "<br>";
+echo str_repeat("=", 100);
+
+// for ($i = 0; $i < 100; $i++) {
+// 	echo "=";
+// }
+
 # Multiline text and line breaks
+$longText = "
+Hello world!
+lorem
+ipsum
+modeo
+";
+echo nl2br($longText);
 
 # Multiline text and reserve html tags
+
+//XSS atack
+echo htmlentities("<script>document.location='https://edu.thedevs.co';</script>");
+echo htmlspecialchars_decode(htmlspecialchars("<script>document.location='https://edu.thedevs.co';</script>"));
 
 // http://php.net/manual/en/ref.strings.php
