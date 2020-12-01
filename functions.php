54f99ca458d0e3e66b0cb0a5e7cca5e8bc774476
abdvkh <e-address@bk.ru>
2020-12-01 21:58:26 +0500

    Functions done; Globals started;

--- functions.php
+++ functions.php
@@ -22,7 +22,18 @@
 // echo "<br>";
 // addition(6, 9);
 
-// variable's scope
+## default function argument
+
+// function sum($first_number = 3, $second_number = 4) {
+// 	echo $first_number + $second_number;
+// }
+
+// sum(); // echo 7
+// sum(5); // echo 9
+// sum( , 5); // causes parse error
+// sum(5, 6); // echo 11
+
+# variable's scope
 /*
 Variable scope — and, in particular, local scope — make your code easier to manage. If all your variables are global, they can be read and changed from anywhere in your script. This can cause chaos in large scripts as many different parts of the script attempt to work with the same variable. By restricting a variable to local scope, you limit the amount of code that can access that variable, making your code more robust, more modular, and easier to debug.
  */
@@ -65,28 +76,85 @@ Variable scope — and, in particular, local scope — make your code easier to 
 // echo $anotherVar . '<br>';
 // echo $thirdVar . '<br>';
 
-function multiplyItself($arr) {
-	for ($i = 0; $i < count($arr); $i++) {
-		$arr[$i] = $arr[$i] * $arr[$i];
-	}
-	print_r($arr);
-}
+// function multiplyItself(&$numbers) {
+// 	for ($i = 0; $i < count($numbers); $i++) {
+// 		$numbers[$i] = $numbers[$i] * $numbers[$i];
+// 	}
+// 	print_r($numbers);
+// }
 
-$numbers = [2, 3, 4];
-print_r($numbers);
+// $numbers = [2, 3, 4];
+// print_r($numbers);
 
-echo "<br>";
+// echo "<br>";
 
-multiplyItself($numbers);
+// multiplyItself($numbers);
 
-echo "<br>";
+// echo "<br>";
 
-print_r($numbers);
+// print_r($numbers);
 
 # returning values
+// function sum($frst, $scnd) {
+// 	echo "Something with $frst and $scnd<br>";
+// 	return $frst + $scnd;
+// 	echo "<br>Another thing";
+// }
+
+// echo "before function<br>";
+// var_dump(sum(4, 5));
+// echo "<br>after function";
 
 # function to get the sum of two numbers
+// function sumOfTwoNumbers($firstNumber, $secondNumber) {
+// 	$sum = 0;
+// 	$sum = $firstNumber + $secondNumber;
+// 	return $sum;
+// }
+
+// function sumOfTwoNumbersWithDefaultArguments($firstNumber = 0, $secondNumber = 0) {
+// 	return $firstNumber + $secondNumber;
+// }
+
+// // echo "<br>";
+// // var_dump(sumOfTwoNumbers()); // will cause the problem
+
+// echo "<br>";
+// var_dump(sumOfTwoNumbersWithDefaultArguments());
+
+// echo "<br>";
+// var_dump(sumOfTwoNumbers(4, 5));
+
+// echo "<br>";
+// echo sumOfTwoNumbers(6, 7);
+
+// $sum = sumOfTwoNumbers(8, 9);
+// echo "<br>";
+// print "$sum";
 
 # function to get the sum of all numbers
+// function sumOfArray($numbers = []) {
+// 	// defining variables
+// 	$sum = 0;
+
+// 	// traversing array and adding element to $sum
+// 	foreach ($numbers as $number) {
+// 		$sum += $number;
+// 	}
+
+// 	// returning the $sum
+// 	return $sum;
+// }
+
+// echo sumOfArray();
+// echo "<br>";
+// echo sumOfArray([1, 6, 9, 10]);
+
+// echo "<br>";
+// $arr = [123, 345, 123];
+// echo sumOfArray($arr);
 
 # arrow functions
+// $sumOfTwo = fn($a, $b) => $a + $b;
+
+// echo $sumOfTwo(5, 6);
---
+++ global-variables.php
@@ -0,0 +1,26 @@
+<?php
+
+# $GLOBALS
+// $arr = ['key' => 'value'];
+// $text = 'Some var';
+
+// function someFunc() {
+// 	$someVar = 'Another var';
+// 	echo $GLOBALS['someVar'];
+// }
+
+// print $text; //Some var
+// echo $GLOBALS['text']; //Some var
+// someFunc(); // error
+// echo $GLOBALS['someVar']; // error
+// echo $GLOBALS['arr']['key']; // value
+
+// echo "<pre>";
+// var_dump($GLOBALS);
+// echo "</pre>";
+
+# $_SERVER
+
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
